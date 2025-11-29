54592f8dfb7ae2889e4dddb5257e3dc40f867303
sheldon <talshel@wix.com>
2025-11-29 15:16:07 +0200

    basic pdf rag

---
+++ pdf-rag.py
@@ -0,0 +1,103 @@
+import ollama
+
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_ollama import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+
+
+
+DOC_PATH = "data/iceberg-specs.pdf"
+MODEL_NAME = "llama3.2"
+EMBEDDING_MODEL = "nomic-embed-text"
+VECTOR_STORE_NAME = "simple-rag"
+
+def ingest_pdf(doc_path: str = DOC_PATH):
+    """Load PDF documents."""
+    loader = UnstructuredPDFLoader(file_path=doc_path)
+    data = loader.load()
+    return data
+
+def split_documents(documents):
+    """Split documents into smaller chunks."""
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1200, chunk_overlap=300)
+    chunks = text_splitter.split_documents(documents)
+    print('done splitting, number of chunks: ', len(chunks))
+    return chunks
+
+def create_vector_db(chunks):
+    """Create a vector database from document chunks."""
+    # Pull the embedding model if not already available
+    ollama.pull(EMBEDDING_MODEL)
+
+    vector_db = Chroma.from_documents(
+        documents=chunks,
+        embedding=OllamaEmbeddings(model=EMBEDDING_MODEL),
+        collection_name=VECTOR_STORE_NAME,
+        # persist_directory is None, so every run will create a new vector database
+        persist_directory=None,
+    )
+    print("Vector database created.")
+    return vector_db
+
+def create_retriever(vector_db, llm):
+    """Create a multi-query retriever."""
+    QUERY_PROMPT = PromptTemplate(
+        input_variables=["question"],
+        template="""You are an AI language model assistant. Your task is to generate five
+different versions of the given user question to retrieve relevant documents from
+a vector database. By generating multiple perspectives on the user question, your
+goal is to help the user overcome some of the limitations of the distance-based
+similarity search. Provide these alternative questions separated by newlines.
+Original question: {question}""",
+    )
+
+    retriever = MultiQueryRetriever.from_llm(
+        vector_db.as_retriever(), llm, prompt=QUERY_PROMPT
+    )
+    print("Retriever created.")
+    return retriever
+
+def create_chain(retriever, llm):
+    """Create the chain"""
+    # RAG prompt
+    template = """Answer the question based ONLY on the following context:
+{context}
+Question: {question}
+"""
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    print("Chain created successfully.")
+    return chain
+def main():
+    data = ingest_pdf(DOC_PATH)
+    chunks = split_documents(data)
+    vector_db = create_vector_db(chunks)
+    llm = ChatOllama(model=MODEL_NAME)
+    retriever = create_retriever(vector_db, llm)
+    chain = create_chain(retriever, llm)
+
+    question = "what's Apache Iceberg? in short"
+
+    # Get the response
+    res = chain.invoke(input=question)
+    print("Response:")
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
